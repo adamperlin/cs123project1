8d823c153c6fe06ec940ccc66ea236e8352b5b89
Adam Perlin <adamp@nanosoft.com>
2018-10-06 19:33:41 -0700

    Refactor, add diver animation

--- project1.js
+++ project1.js
@@ -1,130 +1,23 @@
 "use strict";
 
-var treeDx = -5;
-var treeDy = 0;
-var treeImageWidth = 600, treeImageHeight = 600;
-let diverImageWidth = 565, diverImageHeight = 376;
-
 function setup() {	
 	createCanvas(treeImageWidth + diverImageWidth, treeImageHeight);
-
 	angleMode(DEGREES);
 	noStroke();
-	frameRate(30);
+	frameRate(60);
 }
-
-let diverScene = {
-	renderComponents: (opts) => {
-		// first mountain shape
-		push();
-
-		translate(opts.backgroundOrigin.x, opts.backgroundOrigin.y);
-		scale(opts.backgroundScale);
-		rotate(opts.backgroundRotation);
-		noStroke();
-
-		//background(213, 213, 213);
-
-		fill(213, 213, 213);
-		rect(0, 0, diverImageWidth, treeImageHeight);
-
-		fill(213, 120, 89);
-		quad(0, 0, 7, 0, 56, 140, 0, 376);
-	
-		fill(48, 63, 84);
-		beginShape();
-			vertex(0, 376)
-			vertex(56, 140);
-			vertex(89, 0);
-			vertex(125, 0);
-			vertex(184, 308);
-			vertex(196, 376);
-		endShape();
-	
-		fill(49, 48, 62);
-		triangle(56, 140, 0, 376, 140, 376);
-	
-		fill(181, 188, 204);
-		let leftX = 171;
-		let rightX = 290;
-		triangle(leftX + (rightX - leftX) / 2, 60, leftX, 376, rightX, 376);
-	
-		
-		fill(41, 59, 83);
-		stroke(41, 59, 83);
-		triangle(171, 376, 198, 376, 184, 309);
-	
-		fill(211, 183, 22);
-		triangle(516, 376, 565, 283, 565, 376);
-	
-		pop();
-
-
-		
-		//let diverX = 315, diverY = 269;
-
-		push();
-
-		translate(opts.diverOrigin.x, opts.diverOrigin.y);
-		rotate(opts.diverRotation);
-		scale(opts.diverScale)
-
-		let bodyCorners = [
-			{x: 8, y: 9},
-			{x: -3, y: -7},
-			{x: -4, y: -16},
-			{x: -8, y: -17},
-			{x: 6, y: 10}
-		]
-
-		
-		// Diver
-		fill(209, 194, 125);
-		beginShape();
-			vertex(bodyCorners[0].x, bodyCorners[0].y);
-			bezierVertex(4, 4, 2, -3, bodyCorners[1].x, bodyCorners[1].y);
-			vertex(bodyCorners[2].x, bodyCorners[2].y);
-			vertex(bodyCorners[3].x, bodyCorners[3].y);
-			bezierVertex(-3, -1, 3, 6, bodyCorners[4].x, bodyCorners[4].y);
-		endShape();
-	
-		fill(202, 207, 209);
-		triangle(bodyCorners[2].x, bodyCorners[2].y, -10, -45, bodyCorners[3].x, bodyCorners[3].y);
-		
-		fill(202, 207, 209);
-		//stroke(128);
-		beginShape();
-	//		vertex(bodyCorners[0].x, bodyCorners[0].y);
-			vertex(bodyCorners[4].x, bodyCorners[4].y);
-			bezierVertex(10, 23, 9, 18, 15, 28);
-			bezierVertex(10, 21, 9, 16, bodyCorners[0].x, bodyCorners[0].y);
-		endShape();
-		fill(23, 23, 21);
-		ellipse(11, 4, 8, 8);	
-
-		pop();
-	}
-}
-
-
-/*const drawBuffer = (buffer, scene, pos, opts) => {
-	pixelDensity(1);
-	scene.renderComponents(buffer, opts);
-	image(buffer, pos.x, pos.y);
-}*/
 
 
 const LEFT_BOUND_RESET  = -300;
 class Tree {
-	
-	constructor(treePosX, treePosY, treeSize, dx, dy) {
-		this.treePosX = treePosX;
-		this.treePosY = treePosY;
-		this.originalPosX = treePosX;
-		this.originalPosY = treePosY;
-		this.treeSize = treeSize;
-		this.dx = dx;
-		this.dy = dy;
+	constructor(opts) {
+		this.treePosX = opts.treePosX;
+		this.treePosY = opts.treePosY;
+		this.originalPosX = opts.treePosX;
+		this.originalPosY = opts.treePosY;
+		this.treeSize = opts.treeSize;
+		this.dx = opts.treeDx;
+		this.dy = opts.treeDy;
 
 		this.leftMostX = -78;
 		this.rightMostX = 81;
@@ -239,7 +132,13 @@ class Tree {
 	wrapAroundDuplicate() {
 		if (this.treePosX + this.leftMostX < this.MIN_X && !this.duplicated) {
 			this.duplicated = true;
-			return new Tree(this.MAX_X - this.leftMostX, this.treePosY, this.treeSize, this.dx, this.dy)
+			return new Tree({
+					treePosX: this.MAX_X - this.leftMostX, 
+					treePosY: this.treePosY,
+					treeSize: this.treeSize, 
+					treeDx: this.dx, 
+					treeDy:this.dy
+				})
 		} else {
 			return null;
 		}
@@ -250,96 +149,294 @@ class Tree {
 	}
 }
 
-let diverX = 315, diverY = 269;
+class TreeScene {
+	constructor(opts) {
+		this.trees = [];
+		//console.log("TREE OPTS: ", opts.treeOpts)
+		opts.treeOpts.forEach(treeOpt => {
+			//console.log(treeOpt)
+			let t = new Tree(treeOpt);
+			this.trees.push(t);
+			//console.log(this.trees)
+		})
+		this.duplicates = [];
+		this.sunX = opts.sunX;
+		this.sunY = opts.sunY;
+		this.sunWidth = opts.sunWidth;
+		this.sunHeight = opts.sunHeight;
+		this.textPos = opts.textPos;
+		this.textScale = opts.textScale;
+		this.originX = opts.originX;
+		this.originY = opts.originY;
+		this.imageHeight = opts.imageHeight;
+		this.imageWidth = opts.imageWidth;
+	}
 
-var ANIMATE = true;
-
-var tree1 = new Tree(diverImageWidth + treeImageWidth/2, treeImageHeight, 1, treeDx, treeDy);
-var tree2 = new Tree(diverImageWidth + treeImageWidth/4, treeImageHeight, 0.9, treeDx, treeDy);
-var tree3 = new Tree(diverImageWidth + 3*treeImageWidth/4, treeImageHeight, 0.8, treeDx, treeDy);
-
-var trees = [tree1, tree2, tree3];
-var duplicates = [];
-
-function draw() {
-	//background(213, 213, 213)
-	
-	diverScene.renderComponents({
-		backgroundOrigin: {x: 0, y: 0},
-		backgroundScale: 1,
-		backgroundRotation: 0,
-		diverOrigin: {
-			x: diverX,
-			y: diverY
-		},
-		diverScale: 1,
-		diverRotation: 0
-	});
-
-
-
-
-
-
-
-	// Tree scene 
-
-	//background(245);
-	fill(245);
-	rect(diverImageWidth, 0, treeImageWidth, treeImageHeight);
+	renderComponents() {
+		fill(245);
+		rect(this.originX, this.originY, treeImageWidth, treeImageHeight);
 
 	// scale(random(1,1.002));
 	// translate(random(0,1),random(0,1));
 
-	//sun
-	fill(255,100,30);
-	ellipse(diverImageWidth + treeImageWidth/2*(random(1,1.01)), treeImageHeight/2*(random(1,1.01)), treeImageWidth/3.5, treeImageHeight/4);
+		//sun
+		fill(255,100,30);
+		ellipse(this.originX + this.imageWidth/2 * random(1,1.01), this.imageHeight/2*(random(1,1.01)), this.sunWidth, this.sunHeight);
 
 
 	//tree(width/2, height, 1);
 	//tree(width/4, height, .9);
 	//tree(3*width/4, height, .6);
 
-	for (var i = 0; i < trees.length; i++) {
-		var currentTree = trees[i];
-		var newTree = currentTree.wrapAroundDuplicate();
-		if (newTree !== null) {
-			duplicates[i] = newTree;
+		for (var i = 0; i < this.trees.length; i++) {
+			var currentTree = this.trees[i];
+			var newTree = currentTree.wrapAroundDuplicate();
+			if (newTree !== null) {
+				this.duplicates[i] = newTree;
+			}
+
+			currentTree.drawTree();
+			if (typeof this.duplicates[i] !== 'undefined' && this.duplicates[i] !== null) {
+				this.duplicates[i].drawTree();
+			}
+
+			if (currentTree.offScreen()) {
+				this.trees[i] = this.duplicates[i];
+				this.duplicates[i] = null;
+			}
 		}
-
-		currentTree.drawTree();
-		if (typeof duplicates[i] !== 'undefined' && duplicates[i] !== null) {
-			duplicates[i].drawTree();
-		}
-
-		if (currentTree.offScreen()) {
-			trees[i] = duplicates[i];
-			duplicates[i] = null;
-		}
-
-		
-		console.log(trees.length);
-		console.log(duplicates.length);
-	}	
-
-
-
-
 	//Text
-	push();
-	fill(255,125,125);
-    textSize(treeImageWidth/6.6);
-    textAlign(LEFT);
-    textFont("HELVETICA");
-    textStyle('bold');
-    text("STUBBORN",treeImageWidth/2,120);
-    pop();
+		push();
+		fill(255,125,125);
+		textSize(treeImageWidth/6.6);
+		textAlign(LEFT);
+		textFont("HELVETICA");
+		textStyle('bold');
+		text("STUBBORN",treeImageWidth/2,120);
+		pop();
+	}
 }
 
+class Diver { 
+	constructor(opts) {
+		this.opts = opts;
+		this.animationIncrement = 1;
+		this.direction = 1;
+		this.iterationCount = 0;
+		this.diverInitialVelocity = 2;
+		this.diverStartX = opts.diverOrigin.x;
+		this.diverStartY = opts.diverOrigin.y;
+		this.diverCurrentX = opts.diverOriginX;
+		this.diverCurrentY = opts.diverOriginY;
+		this.diverOrigin = opts.diverOrigin;
+		this.rotation = opts.diverRotation;
+		this.scale = opts.diverScale;
+	}
 
+	yPos() {
+		let t = this.iterationCount/20;
+		let pos = 4.9 * t * t + this.diverInitialVelocity * t + this.diverStartY;
+		//console.log("height: ", height, "pos: ", pos);
+		if (pos > height) {
+			this.direction = -this.direction;
+		}
+		return pos;
+	}
+
+	xPos() {
+		let pos = this.diverStartX + this.iterationCount;
+		if (pos < diverX) {
+			this.direction = -this.direction;
+		}
+		return pos;
+	}
+
+	render() {
+		push();
+
+	    if (!ANIMATE) {
+			this.animationIncrement = 0;
+		} else {
+			this.animationIncrement = 1;
+		}
+		translate(this.xPos(), this.yPos());
+		rotate(this.diverRotation);
+		scale(this.scale)
+
+		let bodyCorners = [
+			{x: 8, y: 9},
+			{x: -3, y: -7},
+			{x: -4, y: -16},
+			{x: -8, y: -17},
+			{x: 6, y: 10}
+		]
+
+		
+		// Diver
+		fill(209, 194, 125);
+		beginShape();
+			vertex(bodyCorners[0].x, bodyCorners[0].y);
+			bezierVertex(4, 4, 2, -3, bodyCorners[1].x, bodyCorners[1].y);
+			vertex(bodyCorners[2].x, bodyCorners[2].y);
+			vertex(bodyCorners[3].x, bodyCorners[3].y);
+			bezierVertex(-3, -1, 3, 6, bodyCorners[4].x, bodyCorners[4].y);
+		endShape();
+	
+		fill(202, 207, 209);
+		triangle(bodyCorners[2].x, bodyCorners[2].y, -10, -45, bodyCorners[3].x, bodyCorners[3].y);
+		
+		fill(202, 207, 209);
+		//stroke(128);
+		beginShape();
+	//		vertex(bodyCorners[0].x, bodyCorners[0].y);
+			vertex(bodyCorners[4].x, bodyCorners[4].y);
+			bezierVertex(10, 23, 9, 18, 15, 28);
+			bezierVertex(10, 21, 9, 16, bodyCorners[0].x, bodyCorners[0].y);
+		endShape();
+		fill(23, 23, 21);
+		ellipse(11, 4, 8, 8);	
+
+		pop();
+		this.iterationCount += this.animationIncrement * this.direction;
+	}
+}
+
+class DiverScene {
+	constructor(opts) {
+		this.diver = new Diver(opts);
+		this.opts = opts;
+	}
+
+	renderComponents() {
+		// first mountain shape
+		push();
+
+		translate(this.opts.backgroundOrigin.x, this.opts.backgroundOrigin.y);
+		scale(this.opts.backgroundScale);
+		rotate(this.opts.backgroundRotation);
+		noStroke();
+
+		//background(213, 213, 213);
+
+		fill(213, 213, 213);
+		rect(0, 0, diverImageWidth, treeImageHeight);
+
+		fill(213, 120, 89);
+		quad(0, 0, 7, 0, 56, 140, 0, 376);
+	
+		fill(48, 63, 84);
+		beginShape();
+			vertex(0, 376)
+			vertex(56, 140);
+			vertex(89, 0);
+			vertex(125, 0);
+			vertex(184, 308);
+			vertex(196, 376);
+		endShape();
+	
+		fill(49, 48, 62);
+		triangle(56, 140, 0, 376, 140, 376);
+	
+		fill(181, 188, 204);
+		let leftX = 171;
+		let rightX = 290;
+		triangle(leftX + (rightX - leftX) / 2, 60, leftX, 376, rightX, 376);
+	
+		
+		fill(41, 59, 83);
+		stroke(41, 59, 83);
+		triangle(171, 376, 198, 376, 184, 309);
+	
+		fill(211, 183, 22);
+		triangle(516, 376, 565, 283, 565, 376);
+	
+		pop();
+		this.diver.render();
+	}
+}
+
+var ANIMATE = false;
+
+const treeDx = -5;
+const treeDy = 0;
+const treeImageWidth = 600, treeImageHeight = 600;
+const diverImageWidth = 565, diverImageHeight = 376;
+
+const diverX = 315;
+const diverY = 269;
+let diverScene = new DiverScene({
+	backgroundOrigin: {x: 0, y: 0},
+	backgroundScale: 1,
+	backgroundRotation: 0,
+	diverOrigin: {
+		x: diverX,
+		y: diverY,
+	},
+	diverScale: 1,
+	diverRotation: 0
+});
+
+const textPosX = treeImageWidth/2, textPosY = 120;
+const textScale = treeImageWidth/6.6;
+const sunX = diverImageWidth + treeImageWidth/2;
+const sunY =  treeImageHeight/2;
+const sunWidth = treeImageWidth/3.5;
+const sunHeight = treeImageHeight/4;
+
+let treeScene = new TreeScene({
+	treeOpts: [
+		{
+			treePosX: diverImageWidth + treeImageWidth/2,
+			treePosY: treeImageHeight, 
+			treeSize: 1, 
+			treeDx: treeDx, 
+			treeDy: treeDy,
+		},
+		{
+			treePosX:diverImageWidth + treeImageWidth/4,
+			treePosY: treeImageHeight,
+			treeSize: 0.9,
+			treeDx: treeDx, 
+			treeDy: treeDy,
+		},
+		{
+			treePosX: diverImageWidth + 3*treeImageWidth/4, 
+			treePosY: treeImageHeight, 
+			treeSize: 0.8,
+			treeDx: treeDx, 
+			treeDy: treeDy,
+		}
+	],
+	textPosX: textPosX,
+	textPosY: textPosY,
+	textScale: textScale,
+	sunWidth: sunWidth,
+	sunHeight: sunHeight,
+	originX: diverImageWidth,
+	originY: 0,
+	imageWidth: treeImageWidth,
+	imageHeight: treeImageHeight
+});
+
+function draw() {
+	//background(213, 213, 213)
+	diverScene.renderComponents();
+	treeScene.renderComponents();
+	// Tree scene 
+	//background(245);
+}
+
+function withinEllipse(x, y, h, k, a, b) {
+	
+	let v = (x - h) ** 2 / (a ** 2) + (y - k) ** 2 / (b ** 2);
+	console.log(v);
+	return v <= 1;
+}
 
 function mouseClicked() {
-	ANIMATE = !ANIMATE;
+	if (withinEllipse(mouseX, mouseY, sunX, sunY, sunWidth/2, sunHeight/2)) {
+		ANIMATE = !ANIMATE;
+	}
 }
 
 
